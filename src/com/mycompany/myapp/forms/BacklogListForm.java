4d29c2f4dc960f9bcacc50b6124d04fb5c1d385f
haykelmkaddem <haykel.mkaddem1@esprit.tn>
2022-01-03 16:46:29 +0100

    last commit

---
+++ src/com/mycompany/myapp/forms/BacklogListForm.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.forms;
+
+import com.codename1.components.SpanLabel;
+import com.codename1.ui.Form;
+import com.codename1.ui.Toolbar;
+import com.codename1.ui.layouts.BoxLayout;
+import com.mycompany.myapp.services.ServiceBacklog;
+
+/**
+ *
+ * @author mehdibehira
+ */
+public class BacklogListForm extends Form {
+        public BacklogListForm(Form previous) {
+        super("Backlog list", BoxLayout.y());
+        
+        Toolbar tb = new Toolbar(true);
+        setToolbar(tb);
+         
+        getTitleArea().setUIID("Container");
+        setTitle("Backlog Index");
+        getContentPane().setScrollVisible(false);
+
+
+        this.add(new SpanLabel(new ServiceBacklog().getAllBacklogs().toString()));
+
+        this.getToolbar().addCommandToLeftBar("Return", null, (evt) -> {
+            previous.showBack();
+        });
+    }
+    
+}
---
+++ src/com/mycompany/myapp/utils/Statics.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.utils;
+
+/**
+ *
+ * @author mehdibehira
+ */
+public class Statics {
+        // a changer selon votre port et ne pas pusher sur git
+       public static final String BASE_URL = "http://localhost";
+}
